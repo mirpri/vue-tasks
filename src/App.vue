a25417edfa686dbfb38dd4419fb2a4764cd4ba9c
mirpri <6shizhuo@gmail.com>
2025-02-05 16:49:25 +0800

    var & loop 4 checkbox

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,31 @@
   <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <div id="app">
     <h1>Tasks</h1>
-        <taskItem name="eat" :done="true"></taskItem>
+        <div>
+            <taskItem v-for="task in tasks" :key="task.id" :id="task.id" :name="task.name" :done="task.done"></taskItem>
+        </div>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import taskItem from './components/taskItem.vue';
+import { nanoid } from 'nanoid'; // import nanoid to generate unique ids
 
 export default {
   name: 'App',
   components: {
     // HelloWorld
-    taskItem
+    taskItem,
+  },
+  data() {
+    return {
+      tasks: [
+        { id: 'task-' + nanoid(), name: 'eat', done: true },
+        { id: 'task-' + nanoid(), name: 'sleep', done: false },
+        { id: 'task-' + nanoid(), name: 'code', done: false },
+      ]
+    }
   }
 }
 </script>
--- src/components/taskItem.vue
+++ src/components/taskItem.vue
@@ -1,19 +1,22 @@
 <template>
     <div>
-        <input type="checkbox" id="task-item" :checked="isDone">
-        <label for="task-item">{{name}}</label>
+        <input type="checkbox" :id="id" :checked="isDone">
+        <label :for="id">{{name}}</label> <!--for attribute means that the label is for the input with the same id-->
     </div>
 </template>
 
 <script>
+
+
 export default {
     props: {
         name: {required: true, type: String},
-        done: {default: false, type: Boolean}
+        done: {default: false, type: Boolean},
+        id: {required:true, type: String}
     },
     data() {
         return {
-            isDone: this.done
+            isDone: this.done,
         }
     }
 };
